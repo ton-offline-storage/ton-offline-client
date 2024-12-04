de1ba259d7891742885a6ad565a2af75e1754df6
mcnckc <s20b1_gusev@179.ru>
2024-12-05 00:35:49 +0300

    update labels + everlasting transactions

--- scripts/index.js
+++ scripts/index.js
@@ -156,7 +156,7 @@ class TxnCreator {
             amount: nanoAmount,
             seqno: seqno,
             payload: this.processCommentBytes(commentBytes),
-            expireAt: (Math.floor(Date.now() / 1e3) + 60 * 60 * 24)
+            expireAt: 0xFFFFFFFF
         });
         const query = await txn.getQuery().then((res) => res).catch((err) => {throw err});
         const boc = await query.toBoc(false);
